3cca9102d39d3a262b95d0e951c0d8dcb5165267
MasterInTheArtOfFailure <136742730+MasterInTheArtOfFailure@users.noreply.github.com>
2023-06-20 14:24:13 +0300

    Time: 122 ms (48.76%), Space: 58 MB (25.47%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -2,20 +2,21 @@ import java.util.Stack;
 
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int n = position.length;
+    int n = position.length;
     int count = 0;
-    Stack<Double> stack = new Stack<>();
+    double[] time = new double[n];
+    for (int i = 0; i < n; i++) {
+        time[i] = (double) (target - position[i]) / speed[i];
+    }
     Map<Integer, Double> map = new TreeMap<>(Collections.reverseOrder());
     for (int i = 0; i < n; i++) {
-        map.put(position[i], (double) (target - position[i]) / speed[i]);
+        map.put(position[i], time[i]);
     }
+    double cur = 0;
     for (double t : map.values()) {
-        while (!stack.isEmpty() && t > stack.peek()) {
-            stack.pop();
-        }
-        if (stack.isEmpty()) {
+        if (t > cur) {
+            cur = t;
             count++;
-            stack.push(t);
         }
     }
     return count;
